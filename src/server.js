d0c56af02bc00aafd65025110b3541df04ec055b
Sandra Hotsyn <sandrahotsy@gmail.com>
2026-02-28 19:38:29 +0200

    TASK 1

--- src/server.js
+++ src/server.js
@@ -2,15 +2,26 @@ import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
 
+import { errorHandler } from './middleware/errorHandler.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { connectMongoDB } from './db/connectMongoDB.js';
+import { logger } from './middleware/logger.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3030;
 
+app.use(logger); 
 app.use(express.json());
 app.use(cors());
 
+
+// 404 — якщо маршрут не знайдено
+app.use(notFoundHandler);
+
+// Error — якщо під час запиту виникла помилка
+app.use(errorHandler);
+
 await connectMongoDB();
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port /localhost:${PORT}`);
 });
